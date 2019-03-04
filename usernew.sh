908d2ab07e8b7c31cf19354e2a8ea50baf342b56
jiraphaty <jiraphattar3@gmail.com>
2019-03-05 05:16:48 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -9,6 +9,14 @@ useradd $user
   if [ ! -z "$password" ]
   then
     echo -e "$password\n$password\n"|passwd
+    read -p "Expire : " day
+    if [ ! -z "$day" ]
+    then
+      echo -e "chage -E $(date -d +30days +%Y-%m-%d) $user"
+    else
+      echo -e "chage -E $(date -d +$daydays +%Y-%m-%d) $user"
+  else
+    echo "Password must not empty"
   fi
 else
    echo "User must not empty"
