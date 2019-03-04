de96dc6da5890743e3c64c92e558853b38268cb6
jiraphaty <jiraphattar3@gmail.com>
2019-03-05 00:44:36 +0700

    Update openvpn-auto-db8.sh

--- openvpn-auto-db8.sh
+++ openvpn-auto-db8.sh
@@ -49,7 +49,7 @@ yes | sudo ufw enable
 # download script
 cd /usr/bin
 wget -O member "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/member.sh"
-wget -O munu "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/menu.sh"
+wget -O menu "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/menu.sh"
 echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
 #echo "* * * * * service dropbear restart" > /etc/cron.d/dropbear
 chmod +x member
