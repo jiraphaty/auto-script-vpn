78dda53b085aaa6c4aaf6fb307d1cc0bac959a2b
jiraphaty <jiraphattar3@gmail.com>
2019-03-09 21:01:23 +0700

    Update install_web.sh

--- openvpnweb/install_web.sh
+++ openvpnweb/install_web.sh
@@ -9,7 +9,7 @@ service apache2 stop
 
 apt-get purge apache2 apache2-utils apache2.2-bin apache2-common
 apt-get remove --purge mysql-server php5-mysql -y
-apt-get autoremove -y
+apt-get autoremove --purge -y
 apt-get autoclean
 rm rm -Rf /etc/apache2 /usr/lib/apache2 /usr/include/apache2
 rm -rf /etc/mysql
@@ -29,11 +29,11 @@ mysqladmin -u root password $password
 apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
 service apache2 restart
 
-#wget -O /etc/apache2/mods-enabled/dir.conf 'https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/openvpnweb/dir.conf'
+wget -O /etc/apache2/mods-enabled/dir.conf 'https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/openvpnweb/dir.conf'
 
-#sudo a2enmod rewrite
-#service apache2 restart
+sudo a2enmod rewrite
+service apache2 restart
 
-#wget -O /etc/apache2/sites-available/000-default.conf 'https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/openvpnweb/000-default.conf'
+wget -O /etc/apache2/sites-available/000-default.conf 'https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/openvpnweb/000-default.conf'
 
-#service apache2 restart
+service apache2 restart
