14f05f6db5fc7812cf169f90e562984985236828
jiraphaty <jiraphattar3@gmail.com>
2019-03-12 16:45:26 +0700

    Update openvpn-auto-ub14.sh

--- openvpn-auto-ub14.sh
+++ openvpn-auto-ub14.sh
@@ -59,6 +59,7 @@ wget -O menu "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master
 wget -O usernew "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/usernew.sh"
 wget -O speedtest "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/speedtest_cli.py"
 wget -O userdel "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/deluser.sh"
+wget -O trial "https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/trial.sh"
 echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
 #echo "* * * * * service dropbear restart" > /etc/cron.d/dropbear
 chmod +x member
@@ -66,6 +67,7 @@ chmod +x menu
 chmod +x usernew
 chmod +x speedtest
 chmod +x userdel
+chmod +x trial
 clear
 
 printf '###############################\n'
