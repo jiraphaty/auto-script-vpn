c6f58a6c2d21f086607bbe19f62c3613c29c3538
jiraphaty <jiraphattar3@gmail.com>
2019-03-12 16:16:31 +0700

    Update openvpn-auto-ub14.sh

--- openvpn-auto-ub14.sh
+++ openvpn-auto-ub14.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #script by jiraphat yuenying for ubuntu 14.04
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 #install openvpn
 
 Y | apt-get purge openvpn easy-rsa;
